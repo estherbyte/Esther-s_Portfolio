2e7c52f08051f3b9faa2976c0624ce933c27222e
Esther Oderinde <estheroderinde2008@gmail.com>
2024-12-11 10:11:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/*const showMore = document.querySelector(".showBtn");
+const more = document.querySelector(".more");
+const showLess = document.querySelector(".showLess");
+
+showMore.addEventListener("click", () => {
+  more.style.display = "block";
+  showMore.innerHTML = `Show Less>`;
+});
+*/
+
